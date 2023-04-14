1a82c3c8f7497d5c099d6315a9a0fed2a2837355
melorichard <richarddemelobezerra@gmail.com>
2023-04-14 14:53:00 -0300

    adicionado nome Joaquim

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá turmaa!");
+        System.out.println("Meu nome é Joaquim!");
     }
 }
